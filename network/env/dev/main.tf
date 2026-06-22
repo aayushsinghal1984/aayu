6682342390ec873ac5b61c1647e21a8230f436b6
Amit Mishra <mishraamitkumar644@gmail.com>
2026-06-22 14:39:04 +0530

    updates

--- network/env/dev/main.tf
+++ network/env/dev/main.tf
@@ -2,7 +2,7 @@ module "resource_group" {
 
   source = "../../modules/resource-group"
 
-  name     = var.resource_group_name
+  name = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
